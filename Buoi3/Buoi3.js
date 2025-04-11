e4b986080c15d12b87d33bb2d9b86a7072eba534
Nightplants <trinhbaohuy14@gmail.com>
2025-04-11 20:00:59 +0700

    first commit

---
+++ Buoi3/Buoi3.js
@@ -0,0 +1,8 @@
+console.log("Javascript")
+
+let friends = 34
+let name = "Trịnh Bảo Huy"
+let age = 11
+let school = "Tiểu học Lômônôxốp"
+
+console.log("Tôi tên là " + name + ", năm nay tôi " + age + ". Tôi đang học ở trường " + school + ". Tôi có " + friends + "người bạn thân.")
