a24e1c955be349ba3b513eb27266105cedc0ce33
Nightplants <trinhbaohuy14@gmail.com>
2025-04-19 09:15:05 +0700

    commit

--- Buoi4/homework/script.js
+++ Buoi4/homework/script.js
@@ -1,1 +1,14 @@
 alert(`Bài 125`);
+a = prompt(`Nhập a: `);
+b = prompt(`Nhập b: `);
+c = prompt(`Nhập c: `);
+let delta = b * b - 4 * a * c;
+if (delta < 0) {
+  alert(`No solution`);
+} else if (delta == 0) {
+  alert(`x1 = x2 = ${-b / (2 * a)}`);
+} else {
+  let x1 = (-b + Math.sqrt(delta)) / (2 * a);
+  let x2 = (-b - Math.sqrt(delta)) / (2 * a);
+  alert(`x1 = ${x1}, x2 = ${x2}`);
+}
