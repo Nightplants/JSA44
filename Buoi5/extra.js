c68d334777a449f9ce3d6ef22b3f19a91a3d1fe9
Nightplants <trinhbaohuy14@gmail.com>
2025-04-25 20:59:17 +0700

    first commit

--- Buoi5/extra.js
+++ Buoi5/extra.js
@@ -1,4 +1,5 @@
 n = Number(prompt(`Nhập n là số giây: `));
+
 if (n > 3600) {
   hours = Math.floor(n / 3600);
   n = n % 3600;
@@ -11,10 +12,12 @@ if (n > 60) {
 } else {
   minutes = `00`;
 }
+
 if (String(hours).length == 1) {
   hours = "0" + String(hours);
 }
 if (String(minutes).length == 1) {
   minutes = "0" + String(minutes);
 }
+
 alert(`${hours}:${minutes}:${n}`);
