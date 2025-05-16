ebb78539f575e0ecc54e2d5a790792999c5050c1
Nightplants <trinhbaohuy14@gmail.com>
2025-05-16 20:05:18 +0700

    commit

---
+++ Buoi7/dom.js
@@ -0,0 +1,6 @@
+let My_name_Element = document.getElementById("My_name");
+let My_game = document.getElementById("minecraft");
+let My_mod = document.getElementById("mod");
+My_name_Element.textContent = "Minecraft"
+My_game.textContent = "Ronando";
+My_mod.textContent = "Messi";
