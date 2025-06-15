9a0a54e82d592e583170ee8240b306a113eadd5b
Nightplants <trinhbaohuy14@gmail.com>
2025-06-15 20:54:55 +0700

    commit

---
+++ CuoiKhoa/Create_acc/script.js
@@ -0,0 +1,34 @@
+const username = document.querySelector(".username");
+const password = document.querySelector(".password");
+const repassword = document.querySelector(".repassword");
+const create_acc = document.querySelector(".create_acc");
+const h2 = document.querySelector("h2");
+
+function ChangeColor() {
+  h2.style.color = randomColor({ hue: "orange", luminosity: "light" });
+}
+
+create_acc.addEventListener("click", function (e) {
+  e.defaultPrevented;
+  if (
+    username.value === "" ||
+    password.value === "" ||
+    repassword.value === ""
+  ) {
+    alert("⚠️Please do not blank the inputs!⚠️");
+    return;
+  } else if (password.value !== repassword.value) {
+    alert("⚠️Password and password check don't the same!⚠️");
+    password.value = "";
+    repassword.value = "";
+    return;
+  } else {
+    localStorage.setItem("Username", username.value);
+    localStorage.setItem("Password", password.value);
+    alert("Create new account complete✅");
+    username.value = "";
+    password.value = "";
+    repassword.value = "";
+    window.location.href = "/CuoiKhoa/Sign_in/index.html";
+  }
+});
--- CuoiKhoa/Sign_in/script.js
+++ CuoiKhoa/Sign_in/script.js
@@ -1,20 +1,29 @@
-const username = document.querySelector(".username").value;
-const password = document.querySelector(".password").value;
+const username = document.querySelector(".username");
+const password = document.querySelector(".password");
 const sign_in = document.querySelector(".sign_in");
+const h2 = document.querySelector("h2");
+
+function ChangeColor() {
+  h2.style.color = randomColor({ hue: "orange", luminosity: "light" });
+}
 
 sign_in.addEventListener("click", function (e) {
-  e.defaultPrevented();
-  console.log("hello");
+  e.defaultPrevented;
+  if (username.value === "" || password.value === "") {
+    alert("⚠️Please do not blank the inputs!⚠️");
+    return
+  }
+  const users = { ...localStorage };
+  if (users.Username !== username.value) {
+    alert(
+      "⚠️Don't find any user, please check the username or create a new account!⚠️"
+    );return
+  } else if (users.Password !== password.value) {
+    alert("⚠️Wrong password!⚠️");return
+  } else {
+    alert("Sign in complete✅")
+    password.value = ""
+    username.value = ""
+    window.location.href = "/CuoiKhoa/Home/index.html"
+  }
 });
-
-function pickColor() {
-  var colors = [
-    "#ff0000",
-    "#00ff00",
-    "#0000ff",
-    "#ff3333",
-    "#ffff00",
-    "#ff6600",
-  ];
-  var random_color = colors[Math.floor(Math.random() * colors.length)];
-}
