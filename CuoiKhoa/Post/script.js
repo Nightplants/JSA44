9df08d266dced15888bec382ccd0310a6d7fd083
Nightplants <trinhbaohuy14@gmail.com>
2025-06-22 22:02:17 +0700

    commit

--- CuoiKhoa/Post/script.js
+++ CuoiKhoa/Post/script.js
@@ -1,7 +1,7 @@
 const upload = document.querySelector(`.upload`);
 const container = document.querySelector(`.container`);
 const fileInput = document.querySelector(`.fileInput`);
-const maxVisable = 4;
+const maxVisible = 4;
 
 upload.addEventListener(`click`, () => {
   fileInput.click();
@@ -10,9 +10,28 @@ upload.addEventListener(`click`, () => {
 fileInput.addEventListener(`change`, () => {
   const files = Array.from(fileInput.files);
   container.innerHTML = ``;
-  files.slice(0, maxVisable).forEach((file, index) => {
+  files.slice(0, maxVisible).forEach((file, index) => {
     const url = URL.createObjectURL(file);
     const wrapper = document.createElement(`div`);
     wrapper.classList.add(`preview`);
+    if (file.type.startWith(`image/`)) {
+      const img = document.createElement(`img`);
+      img.src = url;
+      wrapper.appendChild(img);
+    } else if (file.type.startWith(`video/`)) {
+      const video = document.createElement(`video`);
+      video.src = url;
+      video.controls = false;
+      wrapper.appendChild(video);
+    }
+
+    if (index === maxVisible - 1 && files.length > maxVisible) {
+      const overlay = document.createElement(`div`);
+      overlay.className = `overlay`;
+      overlay.innerText = `+${files.length - maxVisible} more...`;
+      wrapper.appendChild(overlay);
+    }
+
+    container.appendChild(wrapper);
   });
 });
